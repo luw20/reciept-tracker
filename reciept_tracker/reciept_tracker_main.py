af0b47a55dd7c7b2f0dcb451f31136c029aaf29d
Lucy <lucywoodman1@gmail.com>
2025-02-22 15:58:27 -0500

    First pass

---
+++ reciept_tracker/reciept_tracker_main.py
@@ -0,0 +1,11 @@
+import sys
+
+# Defining main function
+def main():
+    print(sys.path)
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
---
+++ reciept_tracker/scanner.py
@@ -0,0 +1,55 @@
+# Import required packages
+import cv2
+import pytesseract
+
+class Scanner:
+    def __init__(self, text = ""):
+         self.text = text
+
+    def captureTextFromVideo(self):    
+         # Mention the installed location of Tesseract-OCR in your system
+         pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+         
+         # Initialize the video capture
+         cap = cv2.VideoCapture(0)  # 0 for the default camera
+         cap.set(3, 640)
+         cap.set(4, 480)
+         
+         
+         while True:
+             # Read a frame from the camera
+             ret, frame = cap.read()
+         
+             # Perform OCR on the frame
+             self.text = pytesseract.image_to_string(frame)
+         
+         # Perform bounding box detection using Tesseract's built-in capabilities
+             d = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
+             n_boxes = len(d['text'])
+             for i in range(n_boxes):
+                 if int(float(d['conf'][i])) > 0:
+                     (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                     frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                     
+             # Display the frame and the recognized text
+             cv2.imshow('Camera', frame)
+             print(self.text)
+         
+             # Exit if 'q' is pressed
+             if cv2.waitKey(1) & 0xFF == ord('q'):
+                 break
+         
+         # Release the video capture and close the window
+         cap.release()
+         cv2.destroyAllWindows()
+         return self.text
+    
+    def getCapturedText(self, captured_text = ""):
+        captured_text = self.captureTextFromVideo()
+        return captured_text
+       
+
+
+
+         
+     
